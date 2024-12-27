cf29e5b46c77ca206293a68538e33740b202f047
zwl <zhangweilong@topscomm.com>
2024-12-27 15:03:20 +0800

    first

---
+++ demo.py
@@ -0,0 +1,11 @@
+'''
+Description: 
+Author: zhangweilong
+Date: 2024-12-27 15:00:42
+LastEditTime: 2024-12-27 15:02:36
+LastEditors: zhangweilong
+'''
+import requests
+
+
+OLLAMA_URL = "http://localhost:11434/api/generate"
